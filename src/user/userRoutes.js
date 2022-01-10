662376da0271e30a1133dece0be7ca5445efea95
Vladislav Pestsov <79399848+AveselsJS@users.noreply.github.com>
2022-01-10 17:19:51 +0200

    validation

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const userService = require('./userService');
 
 router.post('/api/v1/users', async (req, res) => {
+  const user = req.body;
+
+  if (user.username === null) {
+    return res.status(400).send({
+      validationErrors: {
+        username: 'Username cannot be null',
+      },
+    });
+  }
+
   await userService.save(req.body);
   return res.status(200).send({ message: 'user created' });
 });
