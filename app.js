0adcb543d7b8450de25f820464bc5615d2ba31ad
Vladislav Pestsov <79399848+AveselsJS@users.noreply.github.com>
2022-01-10 12:25:05 +0200

    install prettier and eslint

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000 || process.env.PORT;
-const serverHandler = () => {console.log(`Сервер запущен на порте: ${PORT}`)}
+const serverHandler = () => {
+  console.log(`Сервер запущен на порте: ${PORT}`);
+};
 
-app.listen(PORT, serverHandler)
-
+app.listen(PORT, serverHandler);
