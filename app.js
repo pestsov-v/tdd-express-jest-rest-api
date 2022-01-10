9670a6391196c3bef6c59c673e606b6f016945de
Vladislav Pestsov <79399848+AveselsJS@users.noreply.github.com>
2022-01-10 11:09:46 +0200

    creating project

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000 || process.env.PORT;
+const serverHandler = () => {console.log(`Сервер запущен на порте: ${PORT}`)}
+
+app.listen(PORT, serverHandler)
+
