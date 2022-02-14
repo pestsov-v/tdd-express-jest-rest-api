92391da99d79cc82eb9495f143cde7df3f78e81e
Vladislav Pestsov <pestsov.js@gmail.com>
2022-02-15 00:10:25 +0300

    authentification failure

--- __tests__/UserUpdate.spec.js
+++ __tests__/UserUpdate.spec.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/user/userModel');
 const sequelize = require('../src/config/database');
+const bcrypt = require('bcrypt');
 const en = require('../locales/en/translation.json');
 const ru = require('../locales/ru/translation.json');
 
@@ -19,7 +20,19 @@ const putUser = (id = 5, body = null, options = {}) => {
     agent.set('Accept-Language', options.language);
   }
 
+  if (options.auth) {
+    const { email, password } = options.auth;
+    agent.auth(email, password);
+  }
   return agent.send(body);
+};
+
+const activeUser = { username: 'user1', email: 'user1@gmail.com', password: 'P4ssword', inactive: false };
+
+const addUser = async (user = { ...activeUser }) => {
+  const hash = await bcrypt.hash(user.password, 10);
+  user.password = hash;
+  return await User.create(user);
 };
 
 describe('User Update', () => {
@@ -42,4 +55,33 @@ describe('User Update', () => {
       expect(response.body.message).toBe(message);
     }
   );
+
+  it('returns forbidden when request with incorrect email in basic authorization', async () => {
+    await addUser();
+    const response = await putUser(5, null, { auth: { email: 'user1000mail.com', password: 'P4ssword' } });
+    expect(response.status).toBe(403);
+  });
+
+  it('returns forbidden when request with incorrect password in basic authorization', async () => {
+    await addUser();
+    const response = await putUser(5, null, { auth: { email: 'user1000mail.com', password: 'passsword' } });
+    expect(response.status).toBe(403);
+  });
+
+  it('returns forbidden when update request is sent with correct credentials but for different user', async () => {
+    await addUser();
+    const userToBeUpdated = await addUser({ ...activeUser, username: 'user2', email: 'user2@gmail.com' });
+    const response = await putUser(userToBeUpdated.id, null, {
+      auth: { email: 'user1000mail.com', password: 'P4ssword' },
+    });
+    expect(response.status).toBe(403);
+  });
+
+  it('returns forbidden when update request is sent by inactive user with correct credentials for own user', async () => {
+    const innactiveUser = await addUser({ ...activeUser, inactive: true });
+    const response = await putUser(innactiveUser.id, null, {
+      auth: { email: 'user1000mail.com', password: 'P4ssword' },
+    });
+    expect(response.status).toBe(403);
+  });
 });
